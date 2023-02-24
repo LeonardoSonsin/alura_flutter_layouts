03c987431b06d882dc81981c96553e81a13f4331
Leonardo Sonsin <leonardo.sonsin@blink.com.br>
2023-02-24 15:23:49 -0300

    02. Mergulhar em constraints

--- lib/screens/highlights.dart
+++ lib/screens/highlights.dart
@@ -10,17 +10,30 @@ class Highlights extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          return HighlightItem(
-            imageURI: items[index]["image"],
-            itemTitle: items[index]["name"],
-            itemPrice: items[index]["price"],
-            itemDescription: items[index]["description"],
-          );
-        },
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: CustomScrollView(
+        slivers: [
+          const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Text(
+                "Destaques",
+                style: TextStyle(fontFamily: "Caveat", fontSize: 32.0),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+              return HighlightItem(
+                imageURI: items[index]["image"],
+                itemTitle: items[index]["name"],
+                itemPrice: items[index]["price"],
+                itemDescription: items[index]["description"],
+              );
+            }, childCount: items.length),
+          ),
+        ],
       ),
     );
   }
